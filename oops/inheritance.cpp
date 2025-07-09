e2d3b5ef2feda63db75a35b14d4341329e27a66c
9Vaibhav8 <98vaibhavpal@gmail.com>
2025-07-10 01:30:08 +0530

    inheritance

--- oops/inheritance.cpp
+++ oops/inheritance.cpp
@@ -1,7 +1,40 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+class Employee
+{
+public:
+    int id;
+    float salary;
+    Employee(int inpID)
+    {
 
-int main() {
+        id = inpID;
+        salary = 34.0;
+        cout << "Constructor called " << endl;
+    }
+    Employee(){}
+};
 
-return 0;
+// derived class
+// default visibility mode is private - public members of the base class becomes private of the derived class
+// private numbers are never inherited
+// by deafult inheritance is private
+
+class Programmer :  Employee 
+{
+public:
+    Programmer(int inpID)
+    {
+        id = inpID;
+    }
+    int languageCode = 9;
+};
+
+int main()
+{
+
+    Employee rick(1), carlos(2);
+    Programmer skillF(10);
+
+        return 0;
 }
