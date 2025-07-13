2007aa5545b1338618382f126eccfe36f3470d79
9Vaibhav8 <98vaibhavpal@gmail.com>
2025-07-13 12:45:09 +0530

    templates

--- templates.cpp
+++ templates.cpp
@@ -3,21 +3,25 @@ using namespace std;
 
 // tempelates is must for competitive programming
 
+template <class T>
+
 class vector {
-    int *arr;
-    int size;
+   
     public :
+    // int *arr;
+    T * arr ;
+    int size;
     vector (int m) {
 
         size = m;
-        arr = new int[size];
+        arr = new T[size];
 
     }
 
-    int dotproduct(vector &v) {
-        int d = 0;
+    T dotproduct(vector &v) {
+        T d = 0;
         for (int i = 0; i < size; i++) {
-            d += this -> arr[i];
+            d += this -> arr[i] * v.arr[i];
         }
         return d;
 
@@ -27,6 +31,28 @@ class vector {
 int main()
 {
 
+  /*  vector v1(3);
+    v1.arr[0] = 4;
+    v1.arr[1] = 5;
+    v1.arr[2] = 1;
 
+    vector v2(3);
+    v2.arr[0] = 1;
+    v2.arr[1] = 0;
+    v2.arr[2] = 1; */
+
+    vector<float> v1(3);
+    v1.arr[0] = 4.1;
+    v1.arr[1] = 5;
+    v1.arr[2] = 1;
+
+    vector<float> v2(3);
+    v2.arr[0] = 1.6;
+    v2.arr[1] = 0;
+    v2.arr[2] = 1.8;
+    float a = v1.dotproduct(v2);
+    cout << a ;
+
+    
     return 0;
 }
